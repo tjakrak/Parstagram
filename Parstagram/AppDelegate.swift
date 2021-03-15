d08c74a16b99f1eed843d99d89dd7e25f60ca5dc
ryantjakrakartadinata <rgtjakrakartadinata@dons.usfca.edu>
2021-03-15 00:10:24 -0700

    fixed password

--- Parstagram/AppDelegate.swift
+++ Parstagram/AppDelegate.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import Parse
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -14,6 +15,14 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+
+        let parseConfig = ParseClientConfiguration {
+                $0.applicationId = "SpGACSKLPwtDcS1B6kcVHeA95K1fu0U1OhqT5Paw"
+                $0.clientKey = "aEqt7SGv0pI8aLCIUeFUD94JlKiNxb5iNu23Xep0"
+                $0.server = "https://parseapi.back4app.com"
+        }
+        Parse.initialize(with: parseConfig)
+        
         return true
     }
 
